c0d528b70b1b21058b90748810da47b04e539986
Izmar <3707432+izzyreal@users.noreply.github.com>
2026-02-23 20:29:50 +0100

    Bump max artifact size

--- internal/agent/agent_consts.go
+++ internal/agent/agent_consts.go
@@ -2,6 +2,6 @@ package agent
 
 const (
 	maxReportedOutputBytes = 512 * 1024
-	maxArtifactFileBytes   = 50 * 1024 * 1024
+	maxArtifactFileBytes   = 100 * 1024 * 1024
 	maxArtifactsPerJob     = 2500
 )
